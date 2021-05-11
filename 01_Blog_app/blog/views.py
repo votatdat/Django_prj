2fcfe35880c9cafee83a70992bcd4b89b6b9a4c5
Vo Tat Dat <tatdat13@gmail.com>
2021-05-11 11:24:05 +0700

    Added tag

--- 01_Blog_app/blog/views.py
+++ 01_Blog_app/blog/views.py
@@ -1,17 +1,31 @@
 from django.conf.global_settings import EMAIL_HOST_USER
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
